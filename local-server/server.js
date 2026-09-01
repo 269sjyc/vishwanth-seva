48a5a511aa27201b341af48a5ec239c927da1f12
269sjyc <sanjaychauhaniam@gmail.com>
2026-09-01 19:15:15 +0530

    Move Node server to local-server/ so Vercel serves as pure static site

--- local-server/server.js
+++ local-server/server.js
@@ -2,19 +2,17 @@
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname)));
+// Serve static files from parent folder (where index.html and bus.jpeg are)
+app.use(express.static(path.join(__dirname, '..')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log('');
   console.log('  Vishwanath Seva is running!');
-  console.log('');
-  console.log('  Open this in your browser:');
-  console.log('  http://localhost:' + PORT);
-  console.log('');
+  console.log('  Open: http://localhost:' + PORT);
   console.log('  Press Ctrl+C to stop.');
 });
