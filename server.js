2e51dbd834006afe11deeae8966c482dd392448f
269sjyc <sanjaychauhaniam@gmail.com>
2026-09-01 18:24:36 +0530

    Add Vishwanath Seva music player website

---
+++ server.js
@@ -0,0 +1,20 @@
+﻿const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log('');
+  console.log('  Vishwanath Seva is running!');
+  console.log('');
+  console.log('  Open this in your browser:');
+  console.log('  http://localhost:' + PORT);
+  console.log('');
+  console.log('  Press Ctrl+C to stop.');
+});
